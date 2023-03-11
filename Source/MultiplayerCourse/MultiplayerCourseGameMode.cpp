be42491b4a9f208d1cf103cc3b4423fb78de83f9
Titouan Fuchs <titouan.fuchs@gmail.com>
2023-03-11 11:02:04 +0100

    UE5 Project

---
+++ Source/MultiplayerCourse/MultiplayerCourseGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MultiplayerCourseGameMode.h"
+#include "MultiplayerCourseCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AMultiplayerCourseGameMode::AMultiplayerCourseGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
---
+++ Source/MultiplayerCourse/MultiplayerCourseGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MultiplayerCourseGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMultiplayerCourseGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMultiplayerCourseGameMode();
+};
+
+
+
